933872240f458c27c3e16e90ae5e35a2925797a4
Mohamad Sanas <159630755+MohamadSanas@users.noreply.github.com>
2025-10-02 23:56:12 +0530

    Change train method from supervised to unsupervised

--- src/train.py
+++ src/train.py
@@ -1,20 +1,31 @@
-#Train ML model
-import pandas as pd 
+# train.py for isolation processing and model training
+from preprocess import load_and_preprocess
+import pandas as pd
+import numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier 
-from sklearn.metrics import classification_report, confusion_matrix, accuracy_score
-from load_data import load_data 
-from preprocess import preprocess
-import joblib
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import IsolationForest
+from sklearn.metrics import (
+    accuracy_score, precision_score, recall_score, f1_score, roc_auc_score,
+    confusion_matrix, ConfusionMatrixDisplay
+)
+import matplotlib.pyplot as plt
 
-# Load and preprocess data
-data = load_data('data/raw/data.csv')       
 
-# Preprocess the data
-data = preprocess(data) 
-# Define features and target variable
-X = data.drop(columns=['TARGET'])   
+# 1. Load preprocessed dataset
+df = load_and_preprocess("credit_dataset.csv")
 
-#
+# 2. Separate features 
+X = df.values
+
+# 3. Feature scaling
+scaler = StandardScaler()
+X_scaled = scaler.fit_transform(X)
+
+# 4. Train Isolation Forest model
+model = IsolationForest(n_estimator=100, contamination=0.05, random_state=42)
+model.fit(X_scaled)
+
 
 
